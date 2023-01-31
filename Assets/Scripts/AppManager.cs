40e24a5ed98ca99635c4286f54a323a821fc4a35
barretm4 <gamedevcentral@gmail.com>
2023-01-31 03:43:50 -0500

    add back sprite to newentry prefabe, add method to close prefab to appmanager

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class AppManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CloseUIPanel(GameObject objecttoclose)
+    {
+        objecttoclose.SetActive(false);
     }
 }
